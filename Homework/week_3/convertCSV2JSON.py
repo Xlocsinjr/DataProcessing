509222571789246f0e96d3f2a074f31baf3e1336
Xlocsinjr <Xlocsinjr@gmail.com>
2017-11-13 15:58:01 +0100

    Checkpoint save

--- Homework/week_3/convertCSV2JSON.py
+++ Homework/week_3/convertCSV2JSON.py
@@ -1,6 +1,7 @@
 # convertCSV2JSON.py
 #
-# converts a csv file into a json file
+# converts a csv file into a json file and averages the rainfall sum in a day
+# per month
 #
 # Xander Locsin 10722432
 # 13-11-2017
@@ -10,11 +11,19 @@ import json
 
 
 # From: https://stackoverflow.com/questions/19697846/python-csv-to-json
-csvfile = open('KNMI_20161231_reduced.csv', 'r')
+csvfile = open('KNMI_20171112_reduced.csv', 'r')
 jsonfile = open('Rainsum.json', 'w')
 
-fieldnames = ("Station", "Date", "Rainsum")
-reader = csv.DictReader(csvfile, fieldnames)
-for row in reader:
-    json.dump(row, jsonfile)
-    jsonfile.write('\n')
+
+# fieldnames = ("Station", "Date", "Rainsum")
+# reader = csv.DictReader(csvfile, fieldnames)
+# for row in reader:
+#     json.dump(row, jsonfile)
+#     jsonfile.write('\n')
+
+for row in csvfile:
+    new_row = row.split(",")
+    del new_row[0]
+
+    month = new_row
+    print month
