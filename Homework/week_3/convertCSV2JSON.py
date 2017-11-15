dff64f61b23b78dc8743f57726bfa217df1be8a4
Xlocsinjr <Xlocsinjr@gmail.com>
2017-11-15 12:10:44 +0100

    Working conversion

--- Homework/week_3/convertCSV2JSON.py
+++ Homework/week_3/convertCSV2JSON.py
@@ -4,26 +4,31 @@
 # per month
 #
 # Xander Locsin 10722432
-# 13-11-2017
+# 15-11-2017
 # ------------------------------------------
 import csv
 import json
 
-
-# From: https://stackoverflow.com/questions/19697846/python-csv-to-json
+# Opens files
 csvfile = open('KNMI_20171112_reduced.csv', 'r')
 jsonfile = open('Rainsum.json', 'w')
 
-
-# fieldnames = ("Station", "Date", "Rainsum")
-# reader = csv.DictReader(csvfile, fieldnames)
-# for row in reader:
-#     json.dump(row, jsonfile)
-#     jsonfile.write('\n')
-
+# Iterate through every row
 for row in csvfile:
+    dictionary = {}
+
+    # Split row at "," and remove station number
     new_row = row.split(",")
     del new_row[0]
 
-    month = new_row
-    print month
+    # Forms a string for the date to put in the dictionary
+    date_string = new_row[0][:4] + "-" + new_row[0][4:6] + "-" + new_row[0][6:]
+    dictionary["date"] = date_string
+
+    # Calculates rainfall per day in millimeters and adds it to the dictionary
+    rain = int(new_row[1].strip()) * 0.1
+    dictionary["rainfall"] = round(rain, 1)
+
+    # Dumps the dictionary as a row in the json
+    json.dump(dictionary, jsonfile)
+    jsonfile.write("\n")
