54a2bd7e5f0e98385b1bc607da92b4e26f888ad3
Xlocsinjr <Xlocsinjr@gmail.com>
2017-11-17 00:00:18 +0100

    First working chart

--- Homework/week_3/barchart.js
+++ Homework/week_3/barchart.js
@@ -6,82 +6,60 @@
  *
  ****/
 
-var rainfall = [];
-var dates = [];
 
-// http://bl.ocks.org/Jverma/887877fc5c2c2d99be10
+// // load the data
+// d3.json("Rainsum.json", function(error, data) {
+//  i = 0
+//  data.forEach(function(d) {
+//    rainfall[i] = d.rainfall;
+//    dates[i] = d.date;
+//    i++;
+//  });
 
-// load the data
+// sets width and height for the plot
+var width = 960,
+    height = 500;
+
+// Offset magic number to allow negative values into the chart
+var dataOffset = 0.1;
+
+// Linear scale to properly set the length of the bars
+// Range of the bars starts at height since 0 is at the top
+var y = d3.scale.linear()
+  .range([height, 0]);
+
+// Selects the chart in the html and gives it the attributes of width and height
+var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", height);
+
+// Loads in external data
 d3.json("Rainsum.json", function(error, data) {
-  i = 0
-  data.forEach(function(d) {
-    rainfall[i] = d.rainfall;
-    dates[i] = d.date;
-    i++;
-  });
+
+  // Defines domain based on the data
+  y.domain([0, d3.max(data, function(d) {return d.rainfall;}) + dataOffset]);
+
+  // Width of the bar is defined as width divided by the number of data entries
+  var barWidth = width / data.length;
+
+  // Loads in the external data into g elements
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+
+  // Gives the bar a rectangle (so that it's actually a bar)
+  bar.append("rect")
+
+      // gives the rectangle a proper range and  domain
+      .attr("y", function(d) { return y(d.rainfall + dataOffset); })
+      .attr("height", function(d) {return height - y(d.rainfall + dataOffset);})
+      .attr("width", barWidth - 1);
+
+  bar.append("text")
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) { return y(d.rainfall) - 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.rainfall; });
+
 });
-
-// var div = document.createElement("div");
-// div.innerHTML = "Hello, world!";
-// document.body.appendChild(div);
-//
-// var body = d3.select("body");
-// var div = body.append("div");
-// div.html("Hello, world!");
-
-d3.select("body")
-    .style("color", "black")
-    .style("background-color", "white");
-
-d3.selectAll("section")
-    .attr("class", "special")
-  .append("div")
-    .html("Hello, world!");
-
-d3.select("body")
-  .append("div")
-  .html("Hello, world!");
-
-
-
-var data = [4, 8, 15, 16, 23, 42];
-
-d3.select("body")
-    .append("div")
-    .html("Hello, world!");
-
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return d * 10 + "px"; })
-    .text(function(d) { return d; });
-
-d3.select(".chart").selectAll("div")
-    .style("color", "black")
-    .style("background-color", "steelblue")
-    .style("padding", "2px")
-    .style("margin", "2px");
-
-window.onload = function() {
-  console.log(dates);
-  console.log(rainfall);
-
-  // var x = d3.scale.linear()
-  //     .domain([0, d3.max(rainfall)])
-  //     .range([0, 10]);
-  //
-  // d3.select(".chart")
-  //   .selectAll("div")
-  //     .data(rainfall)
-  //   .enter().append("div")
-  //     .style("width", function(d) { return x(d) + "px"; })
-  //     .text(function(d) { return d; });
-}
-
-
-// https://bost.ocks.org/mike/bar/
-// var data = [4, 8, 15, 16, 23, 42];
-
-
-// http://bl.ocks.org/Jverma/887877fc5c2c2d99be10
