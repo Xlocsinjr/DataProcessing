ec774c077c3ad6470abb1b2a535c64e2424f9f29
Xlocsinjr <Xlocsinjr@gmail.com>
2017-11-22 12:50:20 +0100

    checkpoint commit

--- Homework/week_4/svg.js
+++ Homework/week_4/svg.js
@@ -1,4 +1,8 @@
-var test = d3.xml("original_test.svg", "image/svg+xml", function(error, xml) {
+d3.xml("original_test.svg", "image/svg+xml", function(error, xml) {
     if (error) throw error;
     document.body.appendChild(xml.documentElement);
+
+    d3.select("svg")
+      .append("rect")
+        .attr
 });
