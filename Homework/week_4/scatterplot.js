7857cb1f0d3d9cf14487a933812590380161fdbb
Xlocsinjr <Xlocsinjr@gmail.com>
2017-11-23 23:57:03 +0100

    Data retrieval

--- Homework/week_4/scatterplot.js
+++ Homework/week_4/scatterplot.js
@@ -6,6 +6,95 @@
  *
  ****/
 
+var days = 31;
+
+// Function to round numbers off to 1 decimal
+function rounder(number){
+  rounded = Math.round( number * 10 ) / 10;
+  return rounded;
+}
+
+
+// Run script when page is fully loaded
+window.onload = function() {
+
+  var url = "https://raw.githubusercontent.com/Xlocsinjr/DataProcessing/master/Homework/week_4/KNMI_20171031.csv"
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+
+  /*
+  * from example in
+  * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/responseText
+  */
+  var raw_data;
+  request.onload = function () {
+   if (request.readyState === request.DONE) {
+     if (request.status === 200) {
+       raw_data = String(request.responseText);
+       main();
+     }
+   }
+  };
+  request.send();
+  // Run this main function when request was succesful
+  function main() {
+    // Splits the data into separate lines
+    var raw_data_split = raw_data.split("\n");
+
+    var stations = [];
+    var precipitations = [];
+    var windspeeds = [];
+    var temperatures = [];
+
+    var station_check = "";
+    var station_count = 0;
+    var precipitation_sum = 0;
+    var windspeed_sum = 0;
+    var temperature_sum = 0;
+
+    for (var line = 0; line < raw_data_split.length; line++){
+      // Splits the lines by comma
+      line_split = raw_data_split[line].trim();
+      line_split = line_split.split(",");
+
+      // Retrieves information from the line
+      station = line_split[0];
+      windspeed = parseInt(line_split[2]);
+      temperature = parseInt(line_split[3]);
+      precipitation = parseInt(line_split[4]);
+
+      // Reformats information to proper units
+      windspeed = rounder(windspeed * 0.1);
+      temperature = rounder(temperature * 0.1);
+      precipitation = rounder(temperature * 0.1);
+
+      windspeed_sum += windspeed;
+      temperature_sum += temperature;
+      precipitation_sum += precipitation;
+
+      // If data for a month of a station is summed: store averages
+      if ((line + 1) % days == 0){
+        stations[station_count] = station;
+        windspeeds[station_count] = rounder(windspeed_sum / days);
+        temperatures[station_count] = rounder(temperature_sum / days);
+        precipitations[station_count] = rounder(precipitation_sum / days);
+
+        // Reset sums to 0 for summation of next station's data
+        windspeed_sum = 0;
+        temperature_sum = 0;
+        precipitation_sum = 0;
+
+        station_count++;
+      }
+
+    }
+    console.log(stations);
+    console.log(precipitations);
+    console.log(windspeeds);
+    console.log(temperatures);
+  }
+}
+
 
 
 // // Sets the margins for the chart and sets the width and height
