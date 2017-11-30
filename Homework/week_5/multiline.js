84a38afba5172b8bf33df5f74b97a9866413bf2f
Xlocsinjr <Xlocsinjr@gmail.com>
2017-11-30 23:03:42 +0100

    dump

--- Homework/week_5/multiline.js
+++ Homework/week_5/multiline.js
@@ -3,19 +3,30 @@
  *
  * Xander Locsin, 10722432
  *
+ * Code mostly based on this example:
+ * http://bl.ocks.org/d3noob/b3ff6ae1c120eea654b5
  ****/
+
+ /* formats date to easier to read string.
+  *From:https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date */
+// function formatDate(dateVariable) {
+//   var day = dateVariable.getDate();
+//   var month = dateVariable.getMonth();
+//   var year = dateVariable.getFullYear();
+//   return day + "-" + month + "-" + year;
+// };
 
 
 // Sets the margins for the chart and sets the width and height
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
+var margin = {top: 30, right: 30, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// parse time. From: https://bl.ocks.org/mbostock/3883245
-var parseTime = d3.timeParse("%d-%b-%y");
+// Parse the date / time
+var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 // Ordinal scale for the x-axis to display dates
-var x = d3.scaleTime()
+var x = d3.time.scale()
     .range([0, width]);
 
 // Linear scale to properly set the length of the bars
@@ -25,6 +36,7 @@ var y = d3.scale.linear()
 // Defines the x-axis. Placed at the bottom.
 var xAxis = d3.svg.axis()
     .scale(x)
+    .ticks(15)
     .orient("bottom");
 
 // Defines the y-axis. Placed on the left.
@@ -32,13 +44,35 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-// // From: http://bl.ocks.org/Caged/6476579
-// var tip = d3.tip()
-//   .attr('class', 'd3-tip')
-//   //.offset([-10, 0])
-//   .html(function(d) {
-//     return "<div class='tip'>" + d.rainfall + " mm </div>";
-//   })
+/* From: http://bl.ocks.org/Caged/6476579
+ * creates different tips for the different lines */
+var tipMax = d3.tip()
+  .attr('class', 'd3-tip')
+  .html(function(d) {
+    var date = formatDate(d.date);
+    var tiptext = "<div class='tip'> date: " + date + "-"
+      + month + "-" + year + "</div>\n"
+      + "<div class='tip'> temperature: " + d.maximum + "\xB0C </div>\n";
+    return tiptext;
+  });
+
+var tipAv = d3.tip()
+  .attr('class', 'd3-tip')
+  .html(function(d) {
+    var date = formatDate(d.date);
+    var tiptext = "<div class='tip'> date: " + date + " h</div>\n"
+      + "<div class='tip'> temperature: " + d.average + "\xB0C </div>\n";
+    return tiptext;
+  });
+
+var tipMin = d3.tip()
+  .attr('class', 'd3-tip')
+  .html(function(d) {
+    var date = formatDate(d.date);
+    var tiptext = "<div class='tip'> date: " + date + " h</div>\n"
+      + "<div class='tip'> temperature: " + d.minimum + "\xB0C </div>\n";
+    return tiptext;
+  });
 
 // Selects the chart in the html and gives it width and height including margins
 var chart = d3.select(".chart")
@@ -47,49 +81,170 @@ var chart = d3.select(".chart")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// chart.call(tip);
+chart.call(tipMax);
+chart.call(tipAv);
+chart.call(tipMin);
+
 
 // Adds a title to the top of the chart
 chart.append("text")
   .attr("x", width / 2)
-  .attr("y", 20)
+  .attr("y", 0)
   .style("font", "20px sans-serif")
-  .text("Sum of rainfall measured per day at Schiphol from october 30 to november 12 in 2017");
+  .text("Minimum, average and maximum temperatures measured in October");
+
+
+// From: http://bl.ocks.org/d3noob/b3ff6ae1c120eea654b5
+// Define the line
+var maxTempLine = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.maximum); });
+
+var avTempLine = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.average); });
+
+var minTempLine = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.minimum); });
+
+
+queue()
+	.defer(d3.json, 'temperatures240.json')
+	.defer(d3.json, 'temperatures344.json')
+  .defer(d3.json, 'temperatures370.json');
 
 // Loads in external data
 d3.json("temperatures240.json", function(error, data) {
   if (error) throw error;
 
-  // Defines domain based on the data
-  x.domain(data.map(function(d) { return d.date; }));
-  y.domain([-dataOffset, d3.max(data, function(d) {return d.rainfall;})]);
+  // ----------CROSSHAIR-------------------------------------------------------
+
+  // focus tracking. From: http://bl.ocks.org/mikehadlow/93b471e569e31af07cd3
+  var focus = chart.append('g').style('display', 'none');
+  // Adds horizontal line to the crosshair
+  focus.append('line')
+      .attr('id', 'focusLineX')
+      .attr('class', 'focusLine');
+
+  // Adds vertical line to the crosshair
+  focus.append('line')
+      .attr('id', 'focusLineY')
+      .attr('class', 'focusLine');
+
+  // Adds a central circle to the crosshair
+  focus.append('circle')
+      .attr('id', 'focusCircle')
+      .attr('r', 5)
+      .attr('class', 'circle focusCircle');
+
+  // Adds an overlaying rectangle on which to show the crosshair
+  chart.append('rect')
+      .attr('class', 'overlay')
+      .attr('width', width)
+      .attr('height', height)
+      .on('mouseover', function() { focus.style('display', null); })
+      .on('mouseout', function() { focus.style('display', 'none'); })
+      .on('mousemove', function() {
+          var mouse = d3.mouse(this);
+          var mouseDate = xScale.invert(mouse[0]);
+          var i = bisectDate(data, mouseDate); // returns the index to the current data item
+
+          var d0 = data[i - 1]
+          var d1 = data[i];
+          // work out which date value is closest to the mouse
+          var d = mouseDate - d0[0] > d1[0] - mouseDate ? d1 : d0;
+
+          var x = xScale(d[0]);
+          var y = yScale(d[1]);
+
+          focus.select('#focusCircle')
+              .attr('cx', x)
+              .attr('cy', y);
+          focus.select('#focusLineX')
+              .attr('x1', x).attr('y1', yScale(yDomain[0]))
+              .attr('x2', x).attr('y2', yScale(yDomain[1]));
+          focus.select('#focusLineY')
+              .attr('x1', xScale(xDomain[0])).attr('y1', y)
+              .attr('x2', xScale(xDomain[1])).attr('y2', y);
+      });
+
+  // ------------------PATH, DATA AND DOTS--------------------------------------
+
+  // parse date strings to dates
+  data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    });
+
+  // Defines x domain as minimum to maximum of date
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+
+  // Defines y domain as 0 to maximum of maximumt temperatures
+  y.domain([0, d3.max(data, function(d) {return d.maximum;})]);
 
 
-  // Width of the bar is defined as width divided by the number of data entries
-  var barWidth = width / data.length;
+  // Add line for maximum temperature.
+  chart.append("path")
+    .attr("class", "line")
+    .attr("d", maxTempLine(data))
+    .style("stroke", "orange");
 
-  // Loads in the external data into g elements
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("class", "bar")
-      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+  // Add line for average temperature
+  chart.append("path")
+    .attr("class", "line")
+    .attr("d", avTempLine(data))
+    .style("stroke", "grey");
 
-  // Gives the bar a rectangle (so that it's actually a bar)
-  bar.append("rect")
-      // gives the rectangle a proper range and  domain
-      .attr("y", function(d) { return y(d.rainfall + dataOffset); })
+  // Add line for minimum temperature
+  chart.append("path")
+    .attr("class", "line")
+    .attr("d", minTempLine(data))
+    .style("stroke", "steelblue");
 
-      // Set height to the rainfall data + the offset to allow negatives
-      .attr("height", function(d) {return height - y(d.rainfall + dataOffset);})
-
-      // Set width to barWidth - 1 to create space between bars
-      .attr("width", barWidth - 1)
-
+  // Add dots for maximum temperature
+  var dots = chart.append("g");
+  dots.selectAll("lineDots")
+    .data(data)
+    .enter().append("circle")
+      .attr("class", "lineDot")
+      .attr("cx", function (d) { return x(d.date); } )
+      .attr("cy", function (d) { return y(d.maximum); } )
+      .attr("r", 5)
+      .style("fill", "orange")
       // Shows and hides the tooltip
-      .on("mouseover", tip.show)
-      .on("mouseout", tip.hide);
+      .on("mouseover", tipMax.show)
+      .on("mouseout", tipMax.hide);
 
+  // Add dots for average temperature
+  var dots2 = chart.append("g");
+  dots2.selectAll("lineDots2")
+    .data(data)
+    .enter().append("circle")
+      .attr("class", "lineDot")
+      .attr("cx", function (d) { return x(d.date); } )
+      .attr("cy", function (d) { return y(d.average); } )
+      .attr("r", 5)
+      .style("fill", "grey" )
+      // Shows and hides the tooltip
+      .on("mouseover", tipAv.show)
+      .on("mouseout", tipAv.hide);
+
+  // Add dots vor minimum temperature
+  var dots3 = chart.append("g");
+  dots3.selectAll("lineDots3")
+    .data(data)
+    .enter().append("circle")
+      .attr("class", "lineDot")
+      .attr("cx", function (d) { return x(d.date); } )
+      .attr("cy", function (d) { return y(d.minimum); } )
+      .attr("r", 5)
+      .style("fill", "steelblue" )
+      // Shows and hides the tooltip
+      .on("mouseover", tipMin.show)
+      .on("mouseout", tipMin.hide);
+
+
+  //------------------------AXES------------------------------------------------
 
   // Adds a g element for an X axis
   chart.append("g")
@@ -110,9 +265,9 @@ d3.json("temperatures240.json", function(error, data) {
       .call(yAxis)
     .append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
+      .attr("y", -40)
       .attr("dy", ".71em")
       .style("font", "20px sans-serif")
       .style("text-anchor", "end")
-      .text("Rainfall (+ 0.1 mm)");
+      .text("Temperature (\xB0C)");
 });
